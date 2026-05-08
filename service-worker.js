ef75cab2b5c2b28810b391fed5263bef51d8a686
anote2james-cpu <anote2james@gmail.com>
2026-05-07 21:22:20 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
-const CACHE_NAME='fishing-engine-v7-elite-cache-v1';
-const FILES_TO_CACHE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES_TO_CACHE)))});
-self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>key!==CACHE_NAME?caches.delete(key):null))));self.clients.claim()});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>caches.open(CACHE_NAME).then(cache=>{cache.put(event.request,response.clone());return response})).catch(()=>caches.match('./index.html'))))});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('v12').then(c=>c.addAll(['index.html'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
